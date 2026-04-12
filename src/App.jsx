4d6737723ab0984a1d7c60952fe1e786212eb555
Anushka_27 <anushkakhewadia@gmail.com>
2026-04-12 18:42:43 +0530

    hello world

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "./assets/vite.svg";
+// import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  return <div>
+   <div>hello world</div> 
+  </div>;
+}
+
+export default App;
