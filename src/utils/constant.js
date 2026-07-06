db8cd04a468a12970dd1dd2de01f29abac83588d
Anushka_27 <anushkakhewadia@gmail.com>
2026-07-06 19:45:59 +0530

    domain name mapped

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
 
 
 // export const BASE_URL ="https://coderly-backend-rtog.onrender.com"
-export const BASE_URL ="https://coderly-backend-rtog.onrender.com"
+export const BASE_URL ="https://api.codershub.online"
